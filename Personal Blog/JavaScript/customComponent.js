9d4b35464de856f4a3e5cd71c009ab99faafd1ae
FnoizY <fares.bahamdan@gmail.com>
2023-10-08 23:31:28 +0300

    Js_2_Exercises_1

---
+++ Personal Blog/JavaScript/customComponent.js
@@ -0,0 +1,13 @@
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `    
+    <footer>
+      <a href="https://github.com/FnoizY"><img src="Assets/Github-logo.svg" alt=""></a>
+      <a href="Advanced Table.html"><img src="Assets/table-icon.png" alt=""></a>
+      <a href="Hire me.html"><img src="Assets/job-send-icon.png" alt=""></a>
+    </footer>    
+    `;
+  }
+}
+
+customElements.define('main-footer', Footer);
