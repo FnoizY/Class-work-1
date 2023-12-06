9f9b893e3cf46a6c5eac13d41c29be43ce9ac6ef
FnoizY <fares.bahamdan@gmail.com>
2023-12-06 16:13:15 +0300

    BE_3_Excercise_4

--- Personal_Blog/Express/app.js
+++ Personal_Blog/Express/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 
 
 const cssRouter = require("../routes/css");
--- Personal_Blog/routes/contact.js
+++ Personal_Blog/routes/contact.js
@@ -6,6 +6,12 @@ router.get('/Contact%20info.html', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'Contact info.html'));
 });
 
-// Add other route handlers as needed
+router.post('/submit-form', (req, res) => {
+
+  const { name, title, email } = req.body;
+
+  res.send(`Form submitted successfully. Name: ${name}, Title: ${title}, Email: ${email}`);
+});
+
 
 module.exports = router;
