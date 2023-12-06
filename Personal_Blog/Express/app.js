db0deb7a4981c40ed99d9b54a095f7745fa53741
FnoizY <fares.bahamdan@gmail.com>
2023-12-06 16:04:08 +0300

    BE_3_Excercise_3 fixed

--- Personal_Blog/Express/app.js
+++ Personal_Blog/Express/app.js
@@ -2,19 +2,31 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+
+
 const cssRouter = require("../routes/css");
 const assetsRouter = require("../routes/assets");
 const javascriptRouter = require("../routes/javascript");
 const mainRouter = require("../routes/main");
 const contactRouter = require("../routes/contact");
-// Add other route files as needed
+const advancedTable = require("../routes/advanced-table");
+const GOT = require("../routes/game-of-thrones");
+const hireMe = require("../routes/hire-me");
+const ticTacToe = require("../routes/tic-tac-toe");
+
+
 
 app.use('/CSS', cssRouter);
 app.use('/Assets', assetsRouter);
 app.use('/JavaScript', javascriptRouter);
 app.use('/', mainRouter);
 app.use('/Contact%20info.html', contactRouter);
-// Use other routers as needed
+app.use('/Advanced%20Table.html', advancedTable);
+app.use('/gameOfThrones.html', GOT);
+app.use('/Hire%20me.html', hireMe);
+app.use('/tic%20tac%20toe.html', ticTacToe);
+
+
 
 app.all('*', (req, res) => {
   res.status(404).send('<h1>Page Not Found</h1>')
